b75b9013413b6926c0e1f636cd9a9156a5cc66a6
Iury Vasconcelos <iury0393@gmail.com>
2022-08-11 09:16:28 -0300

    Criando o registro do user

--- Flashchat/Controller/RegisterViewController.swift
+++ Flashchat/Controller/RegisterViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
-        self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+        if let email = emailTextfield.text,let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
