28c31607ac41542bb2ccda663fed3d311bb52af6
Iury Vasconcelos <iury0393@gmail.com>
2022-08-11 13:32:10 -0300

    Criando a custom cell

--- Flashchat/Controller/ChatViewController.swift
+++ Flashchat/Controller/ChatViewController.swift
@@ -24,6 +24,8 @@ class ChatViewController: UIViewController {
         tableView.dataSource = self
         navigationItem.hidesBackButton = true
         title = K.titleApp
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -48,11 +50,8 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        
-        var content = cell.defaultContentConfiguration()
-        content.text = messages[indexPath.row].body
-        cell.contentConfiguration = content
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
         
         return cell
     }
