934fce42a3a246b2d1aa24cf4c90265f19962ebb
Iury Vasconcelos <iury0393@gmail.com>
2022-08-11 11:44:39 -0300

    Adicionando cell na tela de chat

--- Flashchat/Controller/ChatViewController.swift
+++ Flashchat/Controller/ChatViewController.swift
@@ -13,8 +13,15 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!!"),
+        Message(sender: "1@2.com", body: "What's up?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         navigationItem.hidesBackButton = true
         title = K.titleApp
     }
@@ -31,4 +38,23 @@ class ChatViewController: UIViewController {
             print("Error signing out: %@", signOutError)
         }
     }
+}
+//MARK: - UITableViewDataSource
+
+extension ChatViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection indexPath: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = messages[indexPath.row].body
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
 }
---
+++ Flashchat/Model/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flashchat
+//
+//  Created by Iury Vasconcelos on 11/08/22.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
