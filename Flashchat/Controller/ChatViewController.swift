cd0e031e807cef364732a027abe26f6dffb12bfd
Iury Vasconcelos <iury0393@gmail.com>
2022-08-11 10:21:47 -0300

    Adicionando opção de logout

--- Flashchat/Controller/ChatViewController.swift
+++ Flashchat/Controller/ChatViewController.swift
@@ -6,19 +6,29 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.hidesBackButton = true
+        title = "⚡️FlashChat"
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
-
+    
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
 }
